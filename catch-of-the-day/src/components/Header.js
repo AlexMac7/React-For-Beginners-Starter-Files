01dd2872ddf831861bb34507eacd7e3c13884abb
Alex MacKenzie <alexbcmac7@gmail.com>
2018-03-19 22:40:28 -0700

    Refactor header into stateless component

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,23 +1,42 @@
 import React from 'react';
 
-class Header extends React.Component {
-    render() {
-        return (
-            <header className="top">
-                <h1>
-                    Catch
-                    <span className="ofThe">
-                        <span className="of"> Of </span>
-                        <span className="the"> The </span>
-                    </span>
-                    Day
-                </h1>
-                <h3 className={"tagline"}>
-                    <span> Fresh Daily </span>
-                </h3>
-            </header>
-        )
-    }
-}
+//arrow function with an implicit return
+const Header = (props) => (
+//const Header = ({ tageline, example }) => ( //can destructure the props into their own variables
+    <header className="top">
+        <h1>
+            Catch
+            <span className="ofThe">
+                <span className="of"> Of </span>
+                <span className="the"> The </span>
+            </span>
+            Day
+        </h1>
+        <h3 className={"tagline"}>
+            <span>{props.tagline}</span>
+            {/*when destructuring you can call by the given name*/}
+            {/*<span>{tagline}</span>*/}
+        </h3>
+    </header>
+);
+
+//this is now a stateless functional component
+// function Header(props) {
+//     return (
+//         <header className="top">
+//             <h1>
+//                 Catch
+//                 <span className="ofThe">
+//                         <span className="of"> Of </span>
+//                         <span className="the"> The </span>
+//                     </span>
+//                 Day
+//             </h1>
+//             <h3 className={"tagline"}>
+//                 <span>{props.tagline}</span>
+//             </h3>
+//         </header>
+//     )
+// }
 
 export default Header;
