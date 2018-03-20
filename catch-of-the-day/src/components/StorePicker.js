92e228b6a6e00cb74ba494e520ba5cf509ce61fd
Alex MacKenzie <alexbcmac7@gmail.com>
2018-03-19 22:15:09 -0700

    Using an App component to hold all others

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-class StorePicker extends React.Component
-{
+class StorePicker extends React.Component {
     render() {
         return (
             <form className={'store-selector'}>
