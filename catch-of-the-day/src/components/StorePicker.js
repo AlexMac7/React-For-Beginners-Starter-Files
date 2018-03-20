0a03d565924920d6ccb238d950363110ae08379c
Alex MacKenzie <alexbcmac7@gmail.com>
2018-03-19 21:50:02 -0700

    html with jsx in first componenet

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,7 +3,15 @@ import React from 'react';
 class StorePicker extends React.Component
 {
     render() {
-        return <p> Hello! </p>
+        return (
+            <form className={'store-selector'}>
+                <h2> Please Enter A Store </h2>
+                <input type={'text'} required placeholder={'Store Name'}/>
+                <button type={'submit'}> Visit Store -></button>
+            </form>
+            // only one element can be return in a render method. A parent element can have as many children as you like though. No sibling elements
+            // so for example, with Grid, you'd use <React.Fragment></React.Fragment> and wrap all the elements with it.  The fragment tag renders out to nothing, nothing will show for it. Now you can have sibling elements.
+        );
     }
 }
 
