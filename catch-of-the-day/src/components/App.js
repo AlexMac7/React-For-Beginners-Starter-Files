c954e3c0b74cf49066d9894a12b78de81c4d2836
Alex MacKenzie <alexbcmac7@gmail.com>
2018-03-21 21:34:15 -0700

    Populate with sample fish
    Created a new method ‘loadSampleFishes’ that sets the state of fishes to data in sampleFishes.js
    Create a new component ‘fish’ that creates a list item with the fish properties
    Now in App.js in the menu the fishes are mapped over rendering a Fish component with its props for each
    Pass the loadSamplesFIsh method as props to the Inventory component and use it in a button

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
     state = {
@@ -17,14 +19,24 @@ class App extends React.Component {
         // third: set new fishes object to state
         this.setState({ fishes: fishes, });
     };
+    loadSampleFishes = () => {
+        this.setState({ fishes: sampleFishes });
+    };
     render() {
         return (
             <div className={"catch-of-the-day"}>
                 <div className={"menu"}>
                     <Header tagline={'Fresh Seafood Market'} />
+                    <ul className={'fishes'}>
+                        {/*React wants a unique key for each element / object so it can quickly find them.  Now for each fish object in state we render out a fish component passing props to it */}
+                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+                    </ul>
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish} />
+                <Inventory
+                    addFish={this.addFish}
+                    loadSampleFishes={this.loadSampleFishes}
+                />
             </div>
         );
     }
