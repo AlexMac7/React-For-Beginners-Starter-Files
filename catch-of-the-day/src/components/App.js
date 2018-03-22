468aab31393f2193ceb56840d88e92631fdfb832
Alex MacKenzie <alexbcmac7@gmail.com>
2018-03-21 20:52:33 -0700

    Working with state to add fish

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,16 +4,29 @@ import Inventory from './Inventory';
 import Order from './Order';
 
 class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {},
+    };
+    addFish = fish => {
+        // to update state, never want to update it directly
+        // first: take a copy of the existing state
+        const fishes = {...this.state.fishes};
+        // second: add new fish to fishes
+        fishes[`fish${Date.now()}`] = fish;
+        // third: set new fishes object to state
+        this.setState({ fishes: fishes, });
+    };
     render() {
         return (
             <div className={"catch-of-the-day"}>
                 <div className={"menu"}>
-                    <Header tagline={'Fresh Seafood Market'}/>
+                    <Header tagline={'Fresh Seafood Market'} />
                 </div>
-                <Inventory />
                 <Order />
+                <Inventory addFish={this.addFish} />
             </div>
-        )
+        );
     }
 }
 
