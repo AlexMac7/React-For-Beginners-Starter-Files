aafd010e58f4c6626c03d9382f42929dc81ba7aa
Alex MacKenzie <alexbcmac7@gmail.com>
2018-05-09 12:58:04 -0700

    Adds new firebase db info

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDm8FJLSl_jZzA78Shpgu_zyw-92TpiraU",
-    authDomain: "catch-of-the-day-alexmac.firebaseapp.com",
-    databaseURL: "https://catch-of-the-day-alexmac.firebaseio.com"
+    apiKey: "AIzaSyANgNkoqoMGkWWvCImLuI--YsNb9Bu9OfE",
+    authDomain: "catch-of-the-day-lex.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-lex.firebaseio.com",
 });
 
 const base = Rebase.createClass(firebaseApp.database());
